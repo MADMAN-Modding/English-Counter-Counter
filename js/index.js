668f578583a411dbd1c0aa7fb0a5a7060b0bb899
MADMAN-Modding <115199645+MADMAN-Modding@users.noreply.github.com>
2023-11-21 14:31:21 +0000

    Fixed the Math

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ function main() {
     // Millis to Date Conversion 86,400,000
     christmas_Days /= 86400000
 
-    // Casts to an Int
-    christmas_Days = parseInt(christmas_Days);
+    // Rounds the Days Up
+    christmas_Days = Math.ceil(christmas_Days);
 
     // Edits the number in HTML
     document.getElementById("christmas_Days").innerHTML = christmas_Days;
@@ -17,8 +17,8 @@ function main() {
     // Millis to Date Conversion 86,400,000
     linux_Days /= 86400000;
 
-    // Casts to an Int
-    linux_Days = parseInt(linux_Days);
+    // Rounds the Day Down
+    linux_Days = Math.floor(linux_Days);
 
     // Edits the number in HTML
     document.getElementById("linux_Days").innerHTML = linux_Days;
@@ -29,8 +29,8 @@ function main() {
     // Millis to Date Conversion 86,400,000
     thanksgiving_Days /= 86400000
 
-    // Casts to an Int
-    thanksgiving_Days = parseInt(thanksgiving_Days);
+    // Rounds Days Up
+    thanksgiving_Days = Math.ceil(thanksgiving_Days)
 
     // Edits the number in HTML
     document.getElementById("thanksgiving_Days").innerHTML = thanksgiving_Days;
